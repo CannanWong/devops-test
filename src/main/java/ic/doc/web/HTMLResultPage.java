9663f537add82df444581d2b5cca953f56b10211
Lara Tse <lst20@cloud-vm-45-94>
2023-01-25 14:56:38 +0000

    download test

--- src/main/java/ic/doc/web/HTMLResultPage.java
+++ src/main/java/ic/doc/web/HTMLResultPage.java
@@ -1,61 +1,60 @@
 package ic.doc.web;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.File;
-import java.io.FileWriter;
-
+import javax.servlet.http.HttpServletResponse;
 
 public class HTMLResultPage implements Page {
 
-    private final String query;
-    private final String answer;
+  private final String query;
+  private final String answer;
 
-    public HTMLResultPage(String query, String answer) {
-        this.query = query;
-        this.answer = answer;
+  public HTMLResultPage(String query, String answer) {
+    this.query = query;
+    this.answer = answer;
+  }
+
+  public void writeTo(HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/html");
+    PrintWriter writer = resp.getWriter();
+
+    // Header
+    writer.println("<html>");
+    writer.println("<head><title>" + query + "</title></head>");
+    writer.println("<body>");
+
+    // Content
+    if (answer == null || answer.isEmpty()) {
+      writer.println("<h1>Sorry</h1>");
+      writer.print("<p>Sorry, we didn't understand <em>" + query + "</em></p>");
+    } else {
+      writer.println("<h1>" + query + "</h1>");
+      writer.println("<p>" + answer.replace("\n", "<br>") + "</p>");
     }
 
-    public void writeTo(HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
+    writer.println("<p><a href=\"/\">Back to Search Page</a></p>");
 
-        // Header
-        writer.println("<html>");
-        writer.println("<head><title>" + query + "</title></head>");
-        writer.println("<body>");
+    // Footer
+    writer.println("</body>");
+    writer.println("</html>");
+  }
 
-        // Content
-        if (answer == null || answer.isEmpty()) {
-            writer.println("<h1>Sorry</h1>");
-            writer.print("<p>Sorry, we didn't understand <em>" + query + "</em></p>");
-        } else {
-            writer.println("<h1>" + query + "</h1>");
-            writer.println("<p>" + answer.replace("\n", "<br>") + "</p>");
-        }
+  public void downloadResults(FileWriter writer) throws IOException {
+    writer.write("<html>" +
+        "<head><title>\"" + query + "\"</title></head>" +
+        "<body>");
 
-        writer.println("<p><a href=\"/\">Back to Search Page</a></p>");
-
-        // Footer
-        writer.println("</body>");
-        writer.println("</html>");
-    
+    if (answer == null || answer.isEmpty()) {
+      writer.write("<p>Sorry\nSorry, we didn't understand " + query + "</em></p>");
+    } else {
+      writer.write("<h1>" + query + "</h1>");
+      writer.write("<p>" + answer.replace("\n", "<br>") + "</p>");
     }
-
-    public void downloadResults() throws IOException {
-	 File results = new File("results.md");
-      	 FileWriter writer = new FileWriter("results.md");
-     	 if (answer == null || answer.isEmpty()) {
-     		 writer.write("# Sorry\nSorry, we didn't understand " + query + "");
-     	 }
-   	 else {
-        	writer.write("# " + query + "\n" + answer);
-      	 }	
-    }
-
-    public void generatePdf() throws IOException {
-	 var processBuilder = new ProcessBuilder();
-	 processBuilder.command("pandoc", "results.md", "--pdf-engine=xelatex", "-o", "results.pdf");
-   }
+    // Footer
+    writer.write("</body>");
+    writer.write("</html>");
+  }
+  
 }
+
